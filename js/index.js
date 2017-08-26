93023d63205513b9271dd87601c7ca9b7a8ba84d
Pratyush Singh <singh.pratyush96@gmail.com>
2017-08-27 04:23:02 +0530

    Change host

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WorldMoodTracker from './components/WorldMoodTracker.js';
 
-export const host = 'http://35.193.41.118:9000';
+export const host = 'http://162.222.180.38';
 
 ReactDOM.render(
     <WorldMoodTracker/>,
